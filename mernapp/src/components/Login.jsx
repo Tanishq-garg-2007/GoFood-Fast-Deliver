a1441b5e05b9d11ececcf4b78dac5d2d7941846c
Tanishq-garg-2007 <iit2024038@iiita.ac.in>
2025-02-24 17:34:43 +0530

    Login.jsx

--- mernapp/src/components/Login.jsx
+++ mernapp/src/components/Login.jsx
@@ -9,7 +9,7 @@ const Login = () => {
     const handleSubmit = async (e) => {
       e.preventDefault();
       try {
-        const response = await fetch("http://localhost:5000/api/loginuser", {
+        const response = await fetch("https://gofood-fast-deliver-backend.onrender.com/api/loginuser", {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
