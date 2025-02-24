6e75cae064e911d90db5ecab3a059fea9792b467
Tanishq-garg-2007 <iit2024038@iiita.ac.in>
2025-02-24 17:35:09 +0530

    Signup.jsx

--- mernapp/src/components/Signup.jsx
+++ mernapp/src/components/Signup.jsx
@@ -10,7 +10,7 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:5000/api/creatuser", {
+      const response = await fetch("https://gofood-fast-deliver-backend.onrender.com/api/creatuser", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
